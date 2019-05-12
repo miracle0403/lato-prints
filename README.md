3270f6c4296d619f023acb0b41449772cd438666
Miracle Lawrence <lawrencemiracle72@gmail.com>
2019-05-12 15:14:16 +0100

    start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lato-prints
+
+this website is made by <a href = "http://elzarime.com.ng">Elzarime</a>
---
+++ views/gh.js
@@ -0,0 +1,54 @@
+class="row"><big>
+        <div class ="col-md-6">
+        	<div class = "row">
+        		<div class = "col-xs-4">
+          			 
+            			
+            					<i class="fas fa-phone fa-stack-1x fa-1.5x text-white"></i>
+            				
+          	</div>
+          	<div class = "col-xs-8 fa-1x">
+          			<a href = "tel: +2347039545778">07039545778</a><br> <a href = "tel: +2347051719145">07051719145</a>
+          		</div>
+          	</div>
+       <div class= " ">
+          	
+            					<i class="fab fa-facebook-f fa-stack-1.5x text-white"></i>
+            
+          	
+          
+          			<a href = "http://facebook.com/latolakprints" target = "_blank"> @latolakprints </a>
+          	
+          </div>
+          			<span class="fa-stack fa-1.5x">
+            					
+            					<i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+            				</span>
+          	
+          			<a href = "http://twitter.com/latolakprints" target = "_blank"> @latolakprints </a>
+          	
+          			<span class="fa-stack fa-1.5x">
+            			
+            					<i class="fab fa-instagram fa-stack-1x" style = "color: violet"></i>
+            				</span>
+          		
+          		
+          			<a href = "http://instagram.com/latolakprints" target = "_blank"> @latolakprints </a>
+          		
+        </div>
+        <div class="col-md-6">
+          	
+          			<span class="fa-stack fa-1.5x">
+            					<i class="fas fa-circle fa-stack-2x text-transparent"></i>
+            					<i class="fas fa-map-marker-alt fa-stack-1x fa-inverse"></i>
+            				</span></big>
+          		
+          
+          			<addr class = "text-white"><i><b>
+          				Suite 3 Eni Plaza,<br>
+          				Plot 7/8 Ilo Awela Road Ota, <br>
+          				Ogun State.</i></b>
+          			</addr>
+          	
+          	</div>
+    

03ecf24aaab4bcfdc1c33a2305a6b076e89532e8
Miracle Lawrence <lawrencemiracle72@gmail.com>
2019-05-21 15:56:02 +0100

    hu

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'LATOLAK PRINTS' });
 });
 
+//get the art page
+router.get('/art-design', function(req, res, next) {
+  res.render('art-design', { title: 'LATOLAK PRINTS', sub: 'ART DESIGN' });
+});
+
+//products
+router.get('/products', function(req, res, next) {
+  res.render('products', { title: 'LATOLAK PRINTS', sub: 'OUR PRODUCTS' });
+});
+
+
+
 //get error page. */
 router.get('/404', function(req, res, next) {
   res.status(404).render('404', { title: 'PAGE NOT FOUND' });
--- views/gh.js
+++ views/gh.js
@@ -1,54 +1,17 @@
-class="row"><big>
-        <div class ="col-md-6">
-        	<div class = "row">
-        		<div class = "col-xs-4">
-          			 
-            			
-            					<i class="fas fa-phone fa-stack-1x fa-1.5x text-white"></i>
-            				
-          	</div>
-          	<div class = "col-xs-8 fa-1x">
-          			<a href = "tel: +2347039545778">07039545778</a><br> <a href = "tel: +2347051719145">07051719145</a>
-          		</div>
-          	</div>
-       <div class= " ">
-          	
-            					<i class="fab fa-facebook-f fa-stack-1.5x text-white"></i>
-            
-          	
-          
-          			<a href = "http://facebook.com/latolakprints" target = "_blank"> @latolakprints </a>
-          	
+<div class="col-md-4 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fas fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="/img/portfolio/fruit-bag.jpg" alt="fruit bags made by latolak prints">
+          </a>
+          <div class="portfolio-caption">
+            <h4>Fruit Shopping Bag</h4><!--
+            <p class="text-muted">HD shopping bag</p>--><br>
+            <div class = "p text-muted text-left">
+           	
+            </div>
           </div>
-          			<span class="fa-stack fa-1.5x">
-            					
-            					<i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-            				</span>
-          	
-          			<a href = "http://twitter.com/latolakprints" target = "_blank"> @latolakprints </a>
-          	
-          			<span class="fa-stack fa-1.5x">
-            			
-            					<i class="fab fa-instagram fa-stack-1x" style = "color: violet"></i>
-            				</span>
-          		
-          		
-          			<a href = "http://instagram.com/latolakprints" target = "_blank"> @latolakprints </a>
-          		
         </div>
-        <div class="col-md-6">
-          	
-          			<span class="fa-stack fa-1.5x">
-            					<i class="fas fa-circle fa-stack-2x text-transparent"></i>
-            					<i class="fas fa-map-marker-alt fa-stack-1x fa-inverse"></i>
-            				</span></big>
-          		
-          
-          			<addr class = "text-white"><i><b>
-          				Suite 3 Eni Plaza,<br>
-          				Plot 7/8 Ilo Awela Road Ota, <br>
-          				Ogun State.</i></b>
-          			</addr>
-          	
-          	</div>
-    
